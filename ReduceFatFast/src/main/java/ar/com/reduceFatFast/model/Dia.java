ba5fa01d63625642471f27c0eb7e40bb4855e08b
Ezequiel Gorandi <ezequielgorandi@gmail.com>
2017-06-14 09:25:38 -0300

    modelo

--- ReduceFatFast/src/main/java/ar/com/reduceFatFast/model/Dia.java
+++ ReduceFatFast/src/main/java/ar/com/reduceFatFast/model/Dia.java
@@ -1,5 +1,40 @@
 package ar.com.reduceFatFast.model;
 
+<<<<<<< HEAD
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.com.reduceFatFast.model.Comida.ComidaDelDia;
+
+public class Dia {
+
+	private List<Comida> comidas = new ArrayList<Comida>();
+	
+	public void setComida(ComidaDelDia comidaDelDia, Comida unaComida) {
+		int n ;
+		switch(comidaDelDia) {
+		case DESAYUNO :
+			n = 1;
+			break; // optional
+		case ALMUERZO :
+			n = 2;
+			break; // optional
+		case MERIENDA :
+			n = 3;
+			break; // optional
+		case CENA :
+			n = 4;
+			break; // optional
+			// You can have any number of case statements.
+		default : // Optional
+			n = 0;
+		}
+		comidas.add(n,unaComida);
+	}
+}
+
+
+=======
 public class Dia {
 
 	Comida desayuno;
@@ -32,3 +67,4 @@ public class Dia {
 		this.cena = cena;
 	}
 }
+>>>>>>> 7a395380006c97e5603c6fcbf251b8a6891f4ae4
--- ReduceFatFast/src/main/java/ar/com/reduceFatFast/model/DietaSemanal.java
+++ ReduceFatFast/src/main/java/ar/com/reduceFatFast/model/DietaSemanal.java
@@ -16,7 +16,11 @@ public class DietaSemanal {
 	}
 	
 	public void agregarComida (Comida unaComida, Integer numeroDeDia, ComidaDelDia comidaDelDia){
+<<<<<<< HEAD
+		dias.get(numeroDeDia).setComida(comidaDelDia, unaComida);
+=======
 		//dias.get(numeroDeDia).setComida(comidaDelDia, unaComida);
+>>>>>>> 7a395380006c97e5603c6fcbf251b8a6891f4ae4
 	}
 	
 }
